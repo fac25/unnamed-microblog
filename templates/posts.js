517de7dc407175d70548f02b5efe254f61f9d043
Manoela <mo.tasheva@gmail.com>
2022-09-21 21:45:33 +0100

    Add handler > > Co-authored-by: Paz <mariapazgar@gmail.com>

--- templates/posts.js
+++ templates/posts.js
@@ -2,14 +2,14 @@ function posts(postsArr) {
   return `${postsArr.map(postItem).join('')}`
 }
 
-function postItem({ name, username, message}) {
+function postItem({ handler, username, message}) {
   const time = new Date();
 
   return `
     <div class='post-item'>
         <div class='profile'>
-            <span class='name'>${name}</span> 
-            <span class='username'>@${username}</span>
+            <span class='username'>${username}</span> 
+            <span class='handler'>@${handler || "anonymoustwooter"}</span>
             <span class='time'> · ${time}</span>
         </div>
         <div class='message'>
