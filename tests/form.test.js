b6cb30428c6cf0204c1037a267e4adefe1f26fe9
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-09-21 15:41:25 +0100

    Created first test for form

---
+++ tests/form.test.js
@@ -0,0 +1,18 @@
+const test = require('node:test')
+const server = require('../src/server.js')
+// const { form } = require('../templates/form.js')
+
+const assert = require('node:assert')
+
+test('form is displayed on page', async () => {
+  const app = server.listen(9876)
+  const response = await fetch('http://localhost:9876')
+  app.close()
+
+  const body = await response.text()
+  assert.match(
+    body,
+    / <label for="username">Username<\/label>/,
+    `Expected html to include <input id="username" name="username"> but received ${body}`
+  )
+})
