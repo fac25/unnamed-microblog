25c0c3506382298db7fee81ba9b3c5315fefc665
patdel0 <zefiru@protonmail.com>
2022-09-21 16:28:02 +0100

    Add posts

---
+++ mockData.js
@@ -0,0 +1,20 @@
+const mockData = [
+  {
+    username: 'Paz',
+    message: 'Lorem ipsum paz',
+  },
+  {
+    username: 'Patrick',
+    message: 'Lorem ipsum patrik',
+  },
+  {
+    username: 'Manoela',
+    message: 'Lorem ipsum manoela',
+  },
+  {
+    username: 'Alex',
+    message: 'Lorem ipsum alex',
+  },
+]
+
+module.exports = mockData
--- src/server.js
+++ src/server.js
@@ -1,11 +1,38 @@
 const express = require('express')
-const { form } = require("../templates/form.js")
-
+const { form } = require('../templates/form.js')
+const posts = require('../templates/posts.js')
+const mockData = require('../mockData.js')
 const server = express()
+const staticHandler = express.static('public')
 
-server.get("/", (request, response) => {
+const postsArr = [...mockData]
 
-    response.send(form())
+const bodyParser = express.urlencoded()
+
+server.use(staticHandler)
+
+server.post('/', bodyParser, (request, response) => {
+  postsArr.push(request.body)
+  response.redirect('/')
+})
+
+server.get('/', (request, response) => {
+  response.send(`       
+        <!doctype html>
+        <html>
+            <head>
+                <title>Twitter clone</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel='stylesheet' href='style.css' />
+                <link rel='stylesheet' href='stylesheets/forms.css' />
+                <link rel='stylesheet' href='stylesheets/posts.css' />
+            </head>
+            <body>
+            ${form()}
+            ${posts(postsArr)}
+            </body>
+        </html>
+    `)
 })
 
 module.exports = server
