7638af443dee5951a8141ddac8bacee0533777de
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-09-22 15:48:03 +0100

    Edited post handler to check values are not empty

---
+++ templates/home.js
@@ -0,0 +1,25 @@
+const { form } = require('./form.js')
+const posts = require('./posts.js')
+
+function home(postsArr, errors = {}) {
+  return `       
+        <!doctype html>
+        <html>
+            <head>
+                <title>Twitter clone</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel='stylesheet' href='style.css' />
+                <link rel='stylesheet' href='stylesheets/form.css' />
+                <link rel='stylesheet' href='stylesheets/posts.css' />
+            </head>
+            <body>
+            ${form()}
+            <div class='container posts'>
+              ${posts(postsArr)}
+            </div>
+            </body>
+        </html>
+    `
+}
+
+module.exports = home
