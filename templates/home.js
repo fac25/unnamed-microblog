e41906815345d0437f14ce90e1d1f8d0a4982de1
Manoela <mo.tasheva@gmail.com>
2022-09-22 01:46:15 +0100

    Add landmarks Closes #31

--- templates/home.js
+++ templates/home.js
@@ -5,23 +5,28 @@ function home(postsArr, errors = {}) {
   return `       
         <!doctype html>
         <html>
-            <head>
-                <title>Twitter clone</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel='stylesheet' href='style.css' />
-                <link rel='stylesheet' href='stylesheets/form.css' />
-                <link rel='stylesheet' href='stylesheets/posts.css' />
-            </head>
-            <body>
-            <div class="container error-container">
-            <p>${errors.username || ''}</p>
-            <p>${errors.message || ''}</p>
-            </div>
-            ${form()}
-            <div class='container posts'>
-              ${posts(postsArr)}
-            </div>
-            </body>
+          <head>
+            <title>Twitter clone</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel='stylesheet' href='style.css' />
+            <link rel='stylesheet' href='stylesheets/form.css' />
+            <link rel='stylesheet' href='stylesheets/posts.css' />
+          </head>
+          <body>
+            <header>
+              <h1>Twooter</h1>
+            </header>
+            <main>
+              <div class="container error-container">
+              <p>${errors.username || ''}</p>
+              <p>${errors.message || ''}</p>
+              </div>
+              ${form()}
+              <div class='container posts'>
+                ${posts(postsArr)}
+              </div>
+            </main>
+          </body>
         </html>
     `
 }
