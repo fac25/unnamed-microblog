22f6042d01e251950acdc9de5e28fb955fd65867
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-09-22 15:01:27 +0100

    Added sanitize function and applied it to form inputs

--- templates/posts.js
+++ templates/posts.js
@@ -2,22 +2,26 @@ function posts(postsArr) {
   return `${postsArr.map(postItem).join('')}`
 }
 
-function postItem({ name, username, message}) {
-  const time = new Date();
+function postItem({ name, username, message }) {
+  const time = new Date()
+  console.log(name, username, message, 'hi')
 
   return `
     <div class='post-item'>
         <div class='profile'>
-            <span class='name'>${name}</span> 
-            <span class='username'>@${username}</span>
+            <span class='name'>${sanitize(name)}</span> 
+            <span class='username'>@${sanitize(username)}</span>
             <span class='time'> · ${time}</span>
         </div>
         <div class='message'>
-          <span>${message}</span>
+          <span>${sanitize(message)}</span>
         </div>
     </div>
     `
 }
 
+function sanitize(unsafe) {
+  return unsafe.replaceAll(/</g, '&lt;')
+}
 
 module.exports = posts
